445f499cedf11afaa32b4faaa81d7b5a4f4f41f3
HitroBro <ghiahitarth@gmail.com>
2026-09-05 17:30:00 +0530

    refactor(upload): permit dotfiles if explicitly uploaded by authenticated user

--- server.py
+++ server.py
@@ -170,7 +170,7 @@ class FileServer(BaseHTTPRequestHandler):
             current_path = None
             if filename:
                 safe = os.path.basename(filename).strip()
-                if safe and not safe.startswith('.'):
+                if safe:
                     current_path = os.path.join(target_dir, safe) # path sanitization
                     current_file = open(current_path, "wb", buffering=64*1024)
                     uploaded_files.append(safe)
